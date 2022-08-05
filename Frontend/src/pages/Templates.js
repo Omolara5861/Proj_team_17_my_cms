939a317283dedc4c95834bf45a0c47485cdadab0
Omolara5861 <debbiegterra@gmail.com>
2022-08-05 18:25:00 +0100

    Template pages structure

--- Frontend/src/pages/Templates.js
+++ Frontend/src/pages/Templates.js
@@ -1,7 +1,50 @@
 import React from 'react'
 
 export default function Templates() {
-  return (
-    <div>Templates</div>
-  )
+    return (
+    <section>
+        <div className='header'>
+        </div>
+        <main>
+            <h1>Browse by Category</h1>
+            <div className='blogs-container'>
+                <div className='title'>
+                    <h3>Blogs</h3>
+                    <a href='#'>See All</a>
+                    < div className='images'>
+                        <img src='' alt='blog-template' />
+                        <img src='' alt='blog-template' />
+                    </div>
+                </div>
+            </div>
+
+            <div className='landing-pages-container'>
+            <div className='title'>
+                    <h3>Landing Pages</h3>
+                    <a href='#'>See All</a>
+                    < div className='images'>
+                        <img src='' alt='landing-page-template' />
+                        <img src='' alt='landing-page-template' />
+                    </div>
+            </div>
+            </div>
+
+            <div className='portfolio'>
+            <div className='title'>
+                    <h3>Portfolio</h3>
+                    <a href='#'>See All</a>
+                    < div className='images'>
+                        <div className='portfolio-template'>
+
+                        </div>
+
+                        <div className='portfolio-template'>
+
+                        </div>
+                    </div>
+            </div>
+            </div>
+        </main>
+    </section>
+)
 }
